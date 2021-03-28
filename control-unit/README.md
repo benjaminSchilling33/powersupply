f67de489a9374d2754fed9152e478cfe6553a1a9
Benjamin Schilling <benjamin.schilling33@gmail.com>
2021-03-28 18:31:06 +0200

    Ad Async Webserver

--- control-unit/README.md
+++ control-unit/README.md
@@ -9,3 +9,13 @@ The control unit contains three tabs:
 1. Display of output voltage and current
 2. Documentation of REST Interface
 3. WiFi settings
+
+## Dependencies
+
+| Library           | Version                                  |
+| ----------------- | ---------------------------------------- |
+| arduino-esp32     | dcff2e9774c1a712e601fd62d08a68a5e74dbc62 |
+| lvgl              | 7.7.2                                    |
+| TFT_eSPI          | 2.3.4                                    |
+| AsyncTCP          | ca8ac5f919d02bea07b474531981ddbfd64de97c |
+| ESPAsyncWebServer | f6fff3f91ebf45b91ca4cff2460d2febd61f1e27 |
--- control-unit/control-unit.ino
+++ control-unit/control-unit.ino
@@ -2,6 +2,8 @@
 #include <TFT_eSPI.h>
 #include <Ticker.h>
 #include <EEPROM.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
 #include "output_tab.h"
 #include "rest_interface_tab.h"
@@ -11,10 +13,19 @@
 #define screenHeight 320
 #define LVGL_TICK_PERIOD 20
 
+// GUI Objects
 TFT_eSPI tft = TFT_eSPI(); /* TFT instance */
 static lv_disp_buf_t disp_buf;
 static lv_color_t buf[LV_HOR_RES_MAX * 10];
 Ticker   lvgl_tick;             /* timer for lvgl */
+
+// REST Interface
+AsyncWebServer server(80);
+const char* PARAM_MESSAGE = "message";
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
 
 /* Display flushing */
 void disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
@@ -127,6 +138,37 @@ void setup()
 
   //Create tab view gui
   create_tab_view();
+
+  // ================= REST Interface =================
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/plain", "Hello, world");
+    });
+
+    // Send a GET request to <IP>/get?message=<message>
+    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+        String message;
+        if (request->hasParam(PARAM_MESSAGE)) {
+            message = request->getParam(PARAM_MESSAGE)->value();
+        } else {
+            message = "No message sent";
+        }
+        request->send(200, "text/plain", "Hello, GET: " + message);
+    });
+
+    // Send a POST request to <IP>/post with a form field message set to <message>
+    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
+        String message;
+        if (request->hasParam(PARAM_MESSAGE, true)) {
+            message = request->getParam(PARAM_MESSAGE, true)->value();
+        } else {
+            message = "No message sent";
+        }
+        request->send(200, "text/plain", "Hello, POST: " + message);
+    });
+
+    server.onNotFound(notFound);
+
+    server.begin();
 }
 
 
